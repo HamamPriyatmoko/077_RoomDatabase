f91359ec3dab74945376bbf0f91c683cfa7ae290
HamamPriyatmoko <hamampriyatmoko81@gmail.com>
2023-12-06 19:41:33 +0700

    menambahkan column

--- app/src/main/java/com/example/roomsiswa/ui/Halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/roomsiswa/ui/Halaman/HalamanEntry.kt
@@ -1,8 +1,11 @@
 package com.example.roomsiswa.ui.Halaman
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import com.example.roomsiswa.R
 import com.example.roomsiswa.model.EntryViewModel
 import com.example.roomsiswa.navigasi.DestinasiNavigasi
@@ -34,4 +37,11 @@ fun FormInputSIswa(
     modifier: Modifier = Modifier,
     onValueChange: (DetailSiswa) -> Unit = {},
     enabled: Boolean = true
-){}
+){
+    Column (
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
+    ){
+
+    }
+}
