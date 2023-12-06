676d368d911e36d5a5e76c0b1de9c05f65aa90dd
HamamPriyatmoko <hamampriyatmoko81@gmail.com>
2023-12-06 18:14:10 +0700

    membuat initializer homeview

--- app/src/main/java/com/example/roomsiswa/model/PenyediaViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/model/PenyediaViewModel.kt
@@ -1,10 +1,14 @@
 package com.example.roomsiswa.model
 
 
+import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.roomsiswa.AplikasiSiswa
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
-
+        initializer {
+            HomeViewModel(AplikasiSiswa().container.repositoriSiswa)
+        }
     }
 }
