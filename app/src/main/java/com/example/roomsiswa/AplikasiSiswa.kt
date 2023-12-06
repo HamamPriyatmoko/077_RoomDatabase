f3887f3e36646604682fec4715d260512227d524
HamamPriyatmoko <hamampriyatmoko81@gmail.com>
2023-12-06 18:03:37 +0700

    menambahkan override

--- app/src/main/java/com/example/roomsiswa/AplikasiSiswa.kt
+++ app/src/main/java/com/example/roomsiswa/AplikasiSiswa.kt
@@ -2,6 +2,7 @@ package com.example.roomsiswa
 
 import android.app.Application
 import com.example.roomsiswa.repositori.ContainerApp
+import com.example.roomsiswa.repositori.ContainerDataApp
 
 class AplikasiSiswa : Application(){
     /**
@@ -9,5 +10,8 @@ class AplikasiSiswa : Application(){
      */
     lateinit var container : ContainerApp
 
-
+    override fun onCreate(){
+        super.onCreate()
+        container = ContainerDataApp(this)
+    }
 }
