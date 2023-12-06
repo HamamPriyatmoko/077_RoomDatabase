ede81447f9ee3237d4919a21de24404e6b95281d
HamamPriyatmoko <hamampriyatmoko81@gmail.com>
2023-12-06 18:22:08 +0700

    membuat function dan edit initializer

--- app/src/main/java/com/example/roomsiswa/model/PenyediaViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/model/PenyediaViewModel.kt
@@ -1,6 +1,8 @@
 package com.example.roomsiswa.model
 
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.roomsiswa.AplikasiSiswa
@@ -8,10 +10,17 @@ import com.example.roomsiswa.AplikasiSiswa
 object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer {
-            HomeViewModel(AplikasiSiswa().container.repositoriSiswa)
+            HomeViewModel(aplikasiSiswa().container.repositoriSiswa)
         }
         initializer {
-            EntryViewModel(AplikasiSiswa().container.repositoriSiswa)
+            EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
         }
     }
 }
+
+/**
+ * Fungsi ekstensi query untuk objekk [Application] dan mengambilkan sebuah instance dari
+ * [AplikasiSiswa]
+ */
+
+fun CreationExtras.aplikasiSiswa():AplikasiSiswa = (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
